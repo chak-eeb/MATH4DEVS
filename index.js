de178cf369646e9cf64b4989974167227bc1ffe5
Chakib Atmane <chakib@Chakibs-MacBook-Pro.local>
2020-09-24 06:29:40 -0400

    display products with title price and count

--- index.js
+++ index.js
@@ -32,5 +32,25 @@ let possibleProducts = [
   "🥦",
 ];
 
-let products = [...Array(5)];
+let products = [...Array(5)].map((_, i) => {
+  return {
+    index: i,
+    title: possibleProducts[roll(0, possibleProducts.length)],
+    price: roll(1, 10, 1).toFixed(2),
+    count: roll(1, 6),
+  };
+});
 console.log(products);
+
+let cartContainer = document.getElementById("ShoppingCart");
+let cartHtml = "";
+
+products.forEach((product) => {
+  cartHtml += `<div class="product">
+  <div> ${product.title}</div>
+  <div> \$${product.price}</div>
+  <div> ${product.count}</div>
+  </div>`;
+});
+
+cartContainer.innerHTML = cartHtml;
